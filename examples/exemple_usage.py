e51a72307e09fca87fe4e349700c6d9736157fab
RYLCODE (Gabriel Roque) <gabrielroque152830@gmail.com>
2025-12-25 15:33:38 -0300

    Add comment for password generation usage
    Add comment explaining password generation

--- examples/exemple_usage.py
+++ examples/exemple_usage.py
@@ -2,3 +2,4 @@ from password_generator import gerar_senha
 
 senha = gerar_senha(64)
 print(senha)
+# Gera uma senha de 64 caracteres com letras, números e símbolos, usando aleatoriedade criptográfica.
